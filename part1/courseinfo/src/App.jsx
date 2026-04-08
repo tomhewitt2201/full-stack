83f94fd250e2c6715f38644b6989cc3fd5f15756
Thomas Hewitt <thomas.hewitt2201@gmail.com>
2026-04-08 13:22:55 +0100

    Remove uneeded files - complete exercise 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,121 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+const Header = props => {
+	return (
+		<h1>{ props.course }</h1>
+	)
 }
+
+const Part = props => {
+	return (
+		<p>{ props.part } { props.exercise }</p>
+	)
+}
+
+const Content = props => {
+	return (
+		<div>
+			<Part part={ props.part1 } exercise={ props.exercise1 } />
+			<Part part={ props.part2 } exercise={ props.exercise2 } />
+			<Part part={ props.part3 } exercise={ props.exercise3 } />
+		</div>
+	)
+}
+
+const Total = props => {
+	return (
+		<p><strong>Number of exercises: { props.exercises }</strong></p>
+	)
+}
+
+const App = () => { 
+	const course = 'Half Stack application development'
+	const part1 = 'Fundamentals of React'
+	const exercises1 = 10
+	const part2 = 'Using props to pass data'
+	const exercises2 = 7
+	const part3 = 'State of a component'
+	const exercises3 = 14
+
+	return (
+		<div>
+			<Header course={ course } />
+			
+			<Content 
+				part1={ part1 } 
+				exercise1={ exercises1 } 
+				part2={ part2 } 
+				exercise2={ exercises2 } 
+				part3={ part3 } 
+				exercise3={ exercises3 } />
+
+			<Total exercises={ exercises1 + exercises2 + exercises3 } />
+		</div>
+	)
+ }
 
 export default App
