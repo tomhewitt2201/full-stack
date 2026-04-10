5a1571df60d0c87d81d7a27604b80eb09093aa98
Thomas Hewitt <thomas.hewitt2201@gmail.com>
2026-04-10 13:20:07 +0100

    add React to app

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,4 @@
-// import { react, useState } from 'react'
+import React, { useState } from 'react'
 
 const Header = ({ course }) => <h1>{ course }</h1>
 const Part = ({ part, exercise }) => <p>{ part } { exercise }</p>
