b6e820d03776a2efce03a442c20b7b25e62023bc
Thomas Hewitt <thomas.hewitt2201@gmail.com>
2026-04-10 13:13:58 +0100

    testing test.jsx

--- part1/courseinfo/src/test.jsx
+++ part1/courseinfo/src/test.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from 'react'
 
+const History = props => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            button press history: { props.allClicks.join(' ') }
+        </div>
+    )
+}
+
+const Button = ({ name, title, onClick }) => <button name={ name } title={ title } onClick={ onClick }>{ name }</button>
+
 const App = () => {
     const [clicks, setClicks] = useState({
         left: 0, right: 0
     })
+
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
 
     const handleClicks = e => {
         const newClicks = {
@@ -11,16 +32,23 @@ const App = () => {
             [e.target.name]: clicks[e.target.name] + 1
         }
 
+        const newTotal = () => total + 1
+
         setClicks(newClicks)
+        setAll(allClicks.concat(e.target.title))
+        setTotal(newTotal)
     }
 
     return (
         <div>
             { clicks.left }
-            <button onClick={ handleClicks } name="left">left</button>
 
-            <button onClick={ handleClicks } name="right">right</button>
-            { clicks.right }
+            { Object.keys(clicks).map(item => <Button key={ item } title={ item[0].toUpperCase() } name={ item } onClick={ handleClicks } />) }
+            
+            { clicks.right }    
+            
+            <History allClicks={ allClicks } />
+            <p>Total { total }</p>
         </div>
     )
 }
