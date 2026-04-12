5ba7c1d6aa856f1833b1c5191a619419fff17377
Thomas Hewitt <thomas.hewitt2201@gmail.com>
2026-04-12 18:32:11 +0100

    add completed task for 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
 
 const Button = ({ name, title, onClick }) => <button name={ name } title={ title } onClick={ onClick }>{ name }</button>
+const Stat = ({ feedback, score }) => <p>{ feedback } { score }</p>
 
 const App = () => {
-    // save clicks of each button to its own state
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+
+    const types = Object.keys(feedback)
+
+	const handleClick = type => setFeedback({ ...feedback, [type]: feedback[type] + 1 })
 
     return (
         <div>
 			<h1>give feedback</h1>
 
-			
+			{/* buttons */}
+			<div className="buttons-wrapper">
+				{ types.map(type => <Button key={ type } name={ type } title={ type[0].toUpperCase() } onClick={ () => handleClick(type) } />) }
+			</div>
+
+			{/* stats */}
+			<h2>statistics</h2>
+
+			<div className="feedback-wrapper">
+				{ types.map(type => <Stat key={ type } feedback={ type } score={ feedback[type] } />) }
+			</div>
         </div>
     )
 }
